da7a80eb6a2ee7f3ffd3fb088a604542bd94741d
hui <232344@qq.com>
2018-07-02 18:12:41 +0800

    第二次提交

--- 03-02/MyImage/core/myimage.py
+++ 03-02/MyImage/core/myimage.py
@@ -48,7 +48,8 @@ class ImageUtils:
             for file_name in os.listdir(self.source_dir):
                 image_path = os.path.join(self.source_dir, file_name)
                 if os.path.isfile(image_path):
-                    if fnmatch.fnmatch(file_name, '*.png') or  fnmatch.fnmatch(file_name, '*.jpg') or  fnmatch.fnmatch(file_name,'*.bmp') or fnmatch.fnmatch(file_name, '*.jpeg'):
+                    if fnmatch.fnmatch(file_name, '*.png') or  fnmatch.fnmatch(file_name, '*.jpg')\
+                     or  fnmatch.fnmatch(file_name,'*.bmp') or fnmatch.fnmatch(file_name, '*.jpeg'):
                         im = Image.open(image_path)
                         file_name = os.path.basename(im.filename)
                         h, w = im.size
@@ -76,13 +77,15 @@ class ImageUtils:
         '''
         if file_name in os.listdir(self.source_dir):
             
-            im = Image.open(os.path.join(BASE_DIR, 'db', 'source', file_name))
+            im = Image.open(os.path.join(self.source_dir, file_name))
             im_new = im.rotate(45)
             if transpose:
                 im_new = im_new.transpose(Image.FLIP_LEFT_RIGHT)
             im_new.show()
             n1, n2 = file_name.split('.')
-            im_new.save(os.path.join(BASE_DIR, 'db', 'target',n1+'-rotate'+'.'+n2))
+            im_new.save(os.path.join(self.target_dir, n1+'-rotate'+'.'+n2))
+        else:
+            print('源路径中不存在该文件')
 
     def cut(self, file_name, w1, h1 , w2, h2):
         """"裁剪图片并保存
@@ -90,20 +93,52 @@ class ImageUtils:
         @w1,h1,w2,h2: 两个点坐标
         """
         if file_name in os.listdir(self.source_dir):
-            im = Image.open(os.path.join(BASE_DIR, 'db', 'source', file_name))
+            im = Image.open(os.path.join(self.source_dir, file_name))
             box =  w1, h1, w2, h2
             im_new = im.crop(box)
             im_new.show()
             n1, n2 = file_name.split('.')
-            im_new.save(os.path.join(BASE_DIR, 'db', 'target',n1+'-crop'+'.'+n2))
+            im_new.save(os.path.join(self.target_dir, n1+'-crop'+'.'+n2))
+        else:
+            print('源路径中不存在该文件')
 
+    def thumbnail(self, file_name, percent = 0.5):
+        '''做一个缩略图，当前路径'''
+        try:
+            if file_name in os.listdir(self.source_dir):        
+                im = Image.open(os.path.join(self.source_dir, file_name))
+                im_new = im.copy()
+                # 获取尺寸
+                w, h = im.size            
+                # 默认缩放到50%
+                im_new.thumbnail((int(w*percent), int(h*percent)))
+                n1, n2 = file_name.split('.')
+                im_new.save(os.path.join(self.target_dir, n1+'-thumb'+'.'+n2))
+            else:
+                print('该文件不存在')
+        except Exception as e:
+            self.log.waring('缩略失败', e)
+
+    def thumbnail_all(self, percent=0.5):
+        '''循环source_dir下所有的图片文件，生成缩略图'''
+        for file in os.listdir(self.source_dir):
+            if fnmatch.fnmatch(file, '*.png') or  fnmatch.fnmatch(file, '*.jpg')\
+             or  fnmatch.fnmatch(file,'*.bmp') or fnmatch.fnmatch(file, '*.jpeg'):                
+                self.thumbnail(file, percent)
+
+    def resize(self, w, h):
+        
+        for file in os.listdir()
+
+    def resize_all(self)
         
 def main():
     pic = ImageUtils()
     # print(pic.source_dir)
     # pic.export_image_info()
     # pic.rotate('1.jpg', 45, True)
-    pic.cut('1.jpg', 200,200,600,600)
-
+    # pic.cut('1.jpg', 200,200,600,600)
+    # pic.thumbnail('1.jpg', 0.5)
+    pic.thumbnail_all()
 if __name__ == '__main__':
     main()
